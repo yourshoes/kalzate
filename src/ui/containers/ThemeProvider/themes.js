d9b1da2761cc84784c0c2783210686522d36d328
Tsur <prucheta@gmail.com>
2017-08-26 15:41:56 +0200

    styles(ThemeProvider): Add light theme

--- src/ui/containers/ThemeProvider/themes.js
+++ src/ui/containers/ThemeProvider/themes.js
@@ -15,9 +15,24 @@ export const dark = {
 };
 export const monokai = {
   app: {
-    bgColor: ' #2d2c2c',
+    bgColor: '#2d2c2c',
     color: '#bbb7b7',
     border: '1px solid rgba(163,168,174,0.1)',
+    padding: '5px',
+  },
+  footer: {
+    color: '#a3a8ae',
+  },
+  sidebar: {
+    color: '#979797',
+    hoverColor: '#d7d7d7',
+  },
+};
+export const light = {
+  app: {
+    bgColor: '#f5f5f5',
+    color: '#9b9b9b',
+    border: '1px solid rgba(163,168,174,0.2)',
     padding: '5px',
   },
   footer: {
