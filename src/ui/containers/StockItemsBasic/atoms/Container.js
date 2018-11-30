714708a5a900d7248a7b986634d8ac20dae24905
Antonio Campos <acamposruiz@gmail.com>
2018-11-30 12:24:56 +0100

    fix(Styles): resolve native wrong style with inputs in firefox browser

--- src/ui/containers/StockItemsBasic/atoms/Container.js
+++ src/ui/containers/StockItemsBasic/atoms/Container.js
@@ -11,6 +11,9 @@ export const Container = styled.section`
   height: 100%;
   padding: ${(props) =>
     props.theme && props.theme.app.padding ? props.theme.app.padding : '0px'};
+  input:required { ${/* This is to prevent Firefox native style */''}
+    box-shadow:none;
+  }
 `;
 
 export default Container;
