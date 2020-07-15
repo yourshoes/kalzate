80f61a2b78f3fed66b8878fbb7be7b0f6e464290
Tsur <prucheta@gmail.com>
2020-07-16 00:12:46 +0200

    Updates

---
+++ demo/precache-manifest.1fffab9c7887fe1ae1d6c0665570ee4c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f2e2b5a7e9c8fdf038",
+    "url": "./static/js/main.72f2e2b5.chunk.js"
+  },
+  {
+    "revision": "0f0a070aeefa4ab7bcf5",
+    "url": "./static/js/runtime~main.0f0a070a.js"
+  },
+  {
+    "revision": "5cc86e490477b6885e99",
+    "url": "./static/css/2.a157e38b.chunk.css"
+  },
+  {
+    "revision": "5cc86e490477b6885e99",
+    "url": "./static/js/2.5cc86e49.chunk.js"
+  },
+  {
+    "revision": "31e1178d07296ad97157",
+    "url": "./static/js/3.31e1178d.chunk.js"
+  },
+  {
+    "revision": "d7eb78b34a4b77247e2d",
+    "url": "./static/js/4.d7eb78b3.chunk.js"
+  },
+  {
+    "revision": "1e34a52ec6a97154f48e",
+    "url": "./static/js/5.1e34a52e.chunk.js"
+  },
+  {
+    "revision": "5f52eeaff1350b5c1871",
+    "url": "./static/js/6.5f52eeaf.chunk.js"
+  },
+  {
+    "revision": "53e1f5dbf060f65be6ee",
+    "url": "./static/js/7.53e1f5db.chunk.js"
+  },
+  {
+    "revision": "67aba137d3b12ae505e9",
+    "url": "./static/js/8.67aba137.chunk.js"
+  },
+  {
+    "revision": "70bfab3f98d3e661b4b4",
+    "url": "./static/js/9.70bfab3f.chunk.js"
+  },
+  {
+    "revision": "d987acb1cd2a894bf0cf",
+    "url": "./static/js/10.d987acb1.chunk.js"
+  },
+  {
+    "revision": "7737d22a46eedfd12087",
+    "url": "./static/js/11.7737d22a.chunk.js"
+  },
+  {
+    "revision": "32c3fa01f63b9e007be0",
+    "url": "./static/js/12.32c3fa01.chunk.js"
+  },
+  {
+    "revision": "260ef443edb4dfd026d82e2b21a4c75c",
+    "url": "./static/media/octicons.260ef443.woff"
+  },
+  {
+    "revision": "7b3fd4b10ef8298c7df5c5acad4b7e42",
+    "url": "./static/media/octicons.7b3fd4b1.eot"
+  },
+  {
+    "revision": "ed294b221e32c83d9e79195d599313d3",
+    "url": "./static/media/octicons.ed294b22.ttf"
+  },
+  {
+    "revision": "9e5cc6ea569143be7be53e6f6c7bcebf",
+    "url": "./static/media/octicons.9e5cc6ea.svg"
+  },
+  {
+    "revision": "9337f8e19e8ddc77692027dc7f63490e",
+    "url": "./static/media/zapatero.9337f8e1.svg"
+  },
+  {
+    "revision": "fcbc7efce1924a1d97f6e60b7ff527b0",
+    "url": "./index.html"
+  }
+];
---
+++ demo/static/js/12.32c3fa01.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{886:function(n,w){}}]);
+//# sourceMappingURL=12.32c3fa01.chunk.js.map
